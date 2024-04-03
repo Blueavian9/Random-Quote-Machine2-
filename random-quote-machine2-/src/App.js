4753b1d38f9141dbd7c9033a242e88de40f9db63
Cesar Aguilar <Blueavian9@gmail.com>
2024-04-03 16:42:45 -0700

    updated commit

--- random-quote-machine2-/src/App.js
+++ random-quote-machine2-/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import QuoteBox from "./components/QuoteBox";
+import QuoteButton from "./components/QuoteButton";
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = async () => {
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
+      setQuote(data.content);
+      setAuthor(data.author);
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+
+  const handleNewQuote = () => {
+    fetchQuote();
+  };
+
+  const handleTweetQuote = () => {
+    return `${quote} - ${author}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <QuoteBox quote={quote} author={author} />
+          <QuoteButton
+            onNewQuote={handleNewQuote}
+            onTweetQuote={handleTweetQuote()}
+          />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
